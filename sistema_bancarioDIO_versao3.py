8a592807e316285450d2df9992c597c9e59fe854
LevyCostta <levicostasouza@hotmail.com>
2025-02-25 20:55:27 -0300

    Classe Conta adicionada juntamente com seus def e metodos

--- sistema_bancarioDIO_versao3.py
+++ sistema_bancarioDIO_versao3.py
@@ -1,4 +1,4 @@
-class Cliente():
+class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
@@ -15,3 +15,63 @@ class PessoaFisica(Cliente):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.cpf = cpf
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = '0001'
+        self._cliente = cliente
+        self._transacoes = Historico()
+
+    @classmethod
+    def nova_conta(cls, numero, cliente):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    @property
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print('\n||| Você Não Possui Saldo insuficiente. |||')
+
+        elif valor > 0:
+            self._saldo -= valor
+            print('\n||| Saque Realizado com Sucesso. |||')
+            return True
+        
+        else:   
+            print('\n||| Valor do Saque Inválido. |||')
+        return False
+        
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print('||| Depósito Realizado com Sucesso. |||')
+        
+        else:
+            print('\n||| Valor do Depósito Inválido. |||')
+            return False
+        
+        return True
