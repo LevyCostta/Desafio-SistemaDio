6674ab9ff61cab841391700ec0ed6baf29fa56c5
LevyCostta <levicostasouza@hotmail.com>
2025-02-25 21:21:31 -0300

    classe deposito adicionada

--- sistema_bancarioDIO_versao3.py
+++ sistema_bancarioDIO_versao3.py
@@ -150,3 +150,17 @@ class Saque(Transacao):
 
         if sucesso_trasacao:
             conta.historico.adicionar_transacao(self)
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_trasacao = conta.depositar(self.valor)
+        
+        if sucesso_trasacao:
+            conta.historico.adicionar_transacao(self)
