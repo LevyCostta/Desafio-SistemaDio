1e0385892e81beff9ec797fe2fc1246c0767a21b
LevyCostta <levicostasouza@hotmail.com>
2025-02-25 21:20:32 -0300

    Classe Saque Adicionada

--- sistema_bancarioDIO_versao3.py
+++ sistema_bancarioDIO_versao3.py
@@ -136,3 +136,17 @@ class Transacao(ABC):
     @abstractmethod
     def registrar(self, conta):
         pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_trasacao = conta.sacar(self.valor)
+
+        if sucesso_trasacao:
+            conta.historico.adicionar_transacao(self)
