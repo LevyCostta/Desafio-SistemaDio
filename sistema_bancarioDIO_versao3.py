f6441803bd668a68f1c0ce26efb46a2213de1658
LevyCostta <levicostasouza@hotmail.com>
2025-02-25 21:12:11 -0300

    Classe ContaCorrente adicionada

--- sistema_bancarioDIO_versao3.py
+++ sistema_bancarioDIO_versao3.py
@@ -75,3 +75,35 @@ class Conta:
             return False
         
         return True
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numero_saques = len(
+            transacao for transacao in self.historico.transacoes if transacao['tipo'] == Saque.__name__
+            )
+        
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saques >= self.limite_saques
+
+        if excedeu_limite:
+            print('\n||| Você Excedeu o Limite de Saque. |||')
+
+        elif excedeu_saques:
+            print('\n||| Você Excedeu o Limite de Saque Máximo. |||')
+
+        else:
+            return super().sacar(valor)
+        
+        return False
+    
+    def __str__(self):
+        return f"""\
+            Agência: \t{self.agencia}
+            C/C: \t\t{self.numero}
+            Titular: \t{self.cliente.nome}
+        """
